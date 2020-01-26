53d63b416c7fa5cdcf5bccaad5a4c56f285299c8
codeandclay <oliver@codeandclay.com>
2020-01-26 23:06:44 +0000

    Remove superfluous index from task list title

--- db/migrate/20200125220746_create_task_lists.rb
+++ db/migrate/20200125220746_create_task_lists.rb
@@ -2,7 +2,7 @@ class CreateTaskLists < ActiveRecord::Migration[6.0]
   def change
     create_table :task_lists, id: :uuid do |t|
       t.timestamps null: false
-      t.string :title, null: false, index: true
+      t.string :title, null: false
       t.text :notes, default: ''
       t.belongs_to :creator, type: :uuid
     end
